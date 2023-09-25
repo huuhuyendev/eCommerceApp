7015088663ff99fcc2310b1cae7b6eb2b3f8119d
huuhuyen <huuhuyen18@gmail.com>
2023-09-25 13:08:53 +0700

    Authentication Screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: appname,
+      title: appName,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.transparent,
