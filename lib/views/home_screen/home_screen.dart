f24870b5661e10fb8c2db783731cf21204a6a425
huuhuyen <huuhuyen18@gmail.com>
2023-10-04 11:05:03 +0700

    done home screen

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -123,56 +123,73 @@ class HomeScreen extends StatelessWidget {
                   ),
                   20.heightBox,
                   SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                    children: List.generate(3, (index) => Column(children: [
-                      featuredButton(
-                        icon: featuredImage1[index],
-                        title: featuredTitle1[index],
-                      ),
-                      10.heightBox,
-                      featuredButton(
-                        icon: featuredImage2[index],
-                        title: featuredTitle2[index],
-                      ),
-
-
-                    ])).toList(),
-                  )),
-                //   featured product
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List.generate(
+                          3,
+                          (index) => Column(children: [
+                                featuredButton(
+                                  icon: featuredImage1[index],
+                                  title: featuredTitle1[index],
+                                ),
+                                10.heightBox,
+                                featuredButton(
+                                  icon: featuredImage2[index],
+                                  title: featuredTitle2[index],
+                                ),
+                              ])).toList(),
+                    ),
+                  ),
+                  //   featured product
                   20.heightBox,
                   Container(
                     padding: const EdgeInsets.all(12),
                     width: double.infinity,
-                    decoration: const BoxDecoration(
-                      color: redColor
-                    ),
+                    decoration: const BoxDecoration(color: redColor),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-
                       children: [
-                        featuredProducts.text
-                            .white
+                        featuredProducts.text.white
                             .size(18)
                             .fontFamily(bold)
                             .make(),
-                            10.heightBox,
-                            SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Row(
-                                children: List.generate(6, (index) => Column(
-                                children: [
-                                Image.asset(imgP1, width: 150, fit : BoxFit.cover,),
-                                10.heightBox,
-                                "Laptop 4GB/64GB".text.fontFamily(semibold).color(darkFontGrey).make(),
-                                10.heightBox,
-                                "\$200".text.fontFamily(semibold).color(darkFontGrey).make(),
-                                ],
-                          
-                              ).box.white.margin(const EdgeInsets.symmetric(horizontal: 4)).roundedSM.padding(const EdgeInsets.all(8)).make())),
-                            )
-                      ],
+                        10.heightBox,
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                              children: List.generate(
+                                  6,
+                                  (index) => Column(
+                                        children: [
+                                          Image.asset(
+                                            imgP1,
+                                            width: 150,
+                                            fit: BoxFit.cover,
+                                          ),
+                                          10.heightBox,
+                                          "Laptop 4GB/64GB"
+                                              .text
+                                              .fontFamily(semibold)
+                                              .color(darkFontGrey)
+                                              .make(),
+                                          10.heightBox,
+                                          "\$200"
+                                              .text
+                                              .fontFamily(semibold)
+                                              .color(redColor)
 
+                                              .make(),
+                                        ],
+                                      )
+                                          .box
+                                          .white
+                                          .margin(const EdgeInsets.symmetric(
+                                              horizontal: 4))
+                                          .roundedSM
+                                          .padding(const EdgeInsets.all(8))
+                                          .make())),
+                        )
+                      ],
                     ),
                   ),
                   20.heightBox,
@@ -191,6 +208,51 @@ class HomeScreen extends StatelessWidget {
                           .rounded
                           .clip(Clip.antiAlias)
                           .margin(const EdgeInsets.symmetric(horizontal: 8))
+                          .make();
+                    },
+                  ),
+                  20.heightBox,
+                  GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: 6,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 8,
+                            crossAxisSpacing: 8,
+                            mainAxisExtent: 300
+                        ),
+                    itemBuilder: (context, index) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            imgP5,
+                            width: 200,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                          const Spacer(),
+                          "Laptop 4GB/64GB"
+                              .text
+                              .fontFamily(semibold)
+                              .color(darkFontGrey)
+                              .make(),
+                          10.heightBox,
+                          "\$600"
+                              .text
+                              .fontFamily(semibold)
+                              .color(redColor)
+                              .make(),
+                        ],
+                      )
+                          .box
+                          .white
+                          .margin(const EdgeInsets.symmetric(
+                          horizontal: 4))
+                          .roundedSM
+                          .padding(const EdgeInsets.all(12))
                           .make();
                     },
                   ),
